09c5e408e5f0f1ea0d52be8e53e90a0c6b0b6821
JWangNov <wangjialun@126.com>
2020-04-16 20:33:39 -0400

    Create quick_start.py

---
+++ quick_start.py
@@ -0,0 +1,26 @@
+import os
+
+from doc_summarizer import DocSummarizer
+
+
+def run():
+    print("Where is your file?")
+    filepath = input("Path: ")
+    if filepath.find(".") == -1:
+        raise Exception("Nein! Nein! Nein! Nein! Nein!\nCannot find suffix!!")
+
+    suffix = os.path.splitext(filepath)[-1]
+
+    if suffix != ".txt" and suffix != ".pdf":
+        raise Exception("Nein! Nein! Nein! Nein! Nein!\nNot a .pdf or .txt file!!")
+
+    smrzr = DocSummarizer()
+    # fixme : teaser cannot work
+    if suffix == ".txt":
+        smrzr.txt(filepath)
+    else:
+        smrzr.pdf(filepath)
+
+
+if __name__ == '__main__':
+    run()
